e734b576a6fa487b1662ac63ef86158891afb5a5
Wed0n <wed0n1213@gmail.com>
2024-05-03 22:17:23 +0800

    fix: fix login endpoint

--- user-service/src/main/java/cuit9622/dms/user/controller/MenuApiController.java
+++ user-service/src/main/java/cuit9622/dms/user/controller/MenuApiController.java
@@ -6,14 +6,12 @@ import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/menu/api")
 public class MenuApiController {
 
     @Resource
--- user-service/src/main/java/cuit9622/dms/user/mapper/MenuMapper.java
+++ user-service/src/main/java/cuit9622/dms/user/mapper/MenuMapper.java
@@ -3,6 +3,7 @@ package cuit9622.dms.user.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import cuit9622.dms.common.entity.Menu;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -16,6 +17,27 @@ import java.util.List;
 @Mapper
 public interface MenuMapper extends BaseMapper<Menu> {
 
+    @Select("select\n" +
+            "\tsm.menu_id,\n" +
+            "\tparent_id,\n" +
+            "\ttitle,\n" +
+            "\tcode,\n" +
+            "\tname,\n" +
+            "\tmenu_url,\n" +
+            "\troute_path,\n" +
+            "\tcomponent_path,\n" +
+            "\t`type`,\n" +
+            "\ticon,\n" +
+            "\tparent_name,\n" +
+            "\torder_num\n" +
+            "from\n" +
+            "\tsys_user_role sur\n" +
+            "inner join sys_role_menu srm on\n" +
+            "\tsrm.role_id = sur.role_id\n" +
+            "inner join sys_menu sm on\n" +
+            "\tsm.menu_id = srm.menu_id\n" +
+            "where\n" +
+            "\tsur.user_id = #{userId}")
     List<Menu> getMenuByUserId(Integer userId);
 }
 
