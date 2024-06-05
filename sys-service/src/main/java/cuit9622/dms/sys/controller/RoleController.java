026c2281c8b78015b3d4aee733cd6a15d73e4db4
lsh <3040114965@qq.com>
2024-06-05 22:06:18 +0800

    feat：分页查询角色信息

--- sys-service/src/main/java/cuit9622/dms/sys/controller/RoleController.java
+++ sys-service/src/main/java/cuit9622/dms/sys/controller/RoleController.java
@@ -1,18 +1,19 @@
 package cuit9622.dms.sys.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import cuit9622.dms.common.entity.Role;
+import cuit9622.dms.common.entity.User;
 import cuit9622.dms.common.model.CommonResult;
+import cuit9622.dms.sys.Vo.UserVo;
 import cuit9622.dms.sys.mapper.RoleMapper;
 import cuit9622.dms.sys.mapper.UserMapper;
 import cuit9622.dms.sys.service.RoleService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,19 +29,9 @@ public class RoleController {
     @Resource
     RoleMapper roleMapper;
 
-
-    /**
-     * 查询所有角色
-     * @return
-     */
-    @GetMapping("/list")
-    @PreAuthorize("hasAuthority('sys:role:index')")
-    public CommonResult<List<Role>> getRoles() {
-        return CommonResult.success(roleService.list());
-    }
-
     /**
      * 查询当前用户是什么角色
+     *
      * @param id
      * @return
      */
@@ -50,4 +41,17 @@ public class RoleController {
         Role role = roleMapper.getRoleByUserID(id);
         return CommonResult.success(role);
     }
+
+    /**
+     * 搜索查询所有角色
+     */
+    @GetMapping("/list")
+    @PreAuthorize("hasAuthority('sys:role:index')")
+    public CommonResult<Page<Role>> search(@RequestParam int page, @RequestParam int pageSize, @RequestParam(required = false) String roleName) {
+        LambdaQueryWrapper<Role> wrapper = new LambdaQueryWrapper<>();
+        wrapper.like(roleName != null, Role::getRoleName, roleName);
+        Page<Role> result = roleService.page(new Page<>(page, pageSize), wrapper);
+        return CommonResult.success(result);
+    }
+
 }
