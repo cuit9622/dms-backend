31f989b52052b2c0164cb73adf309c59210df418
lsh <3040114965@qq.com>
2024-06-05 23:14:42 +0800

    feat: 初始化增删改

--- sys-service/src/main/java/cuit9622/dms/sys/controller/RoleController.java
+++ sys-service/src/main/java/cuit9622/dms/sys/controller/RoleController.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import cuit9622.dms.common.entity.Role;
 import cuit9622.dms.common.entity.User;
+import cuit9622.dms.common.exception.BizException;
 import cuit9622.dms.common.model.CommonResult;
 import cuit9622.dms.sys.Vo.UserVo;
 import cuit9622.dms.sys.mapper.RoleMapper;
@@ -43,15 +44,52 @@ public class RoleController {
     }
 
     /**
-     * 搜索查询所有角色
+     * 查询所有角色
      */
     @GetMapping("/list")
     @PreAuthorize("hasAuthority('sys:role:index')")
-    public CommonResult<Page<Role>> search(@RequestParam int page, @RequestParam int pageSize, @RequestParam(required = false) String roleName) {
+    public CommonResult<List<Role>> list() {
+        return CommonResult.success(roleService.list());
+    }
+
+    /**
+     * 搜索查询所有角色
+     */
+    @GetMapping("/page")
+    @PreAuthorize("hasAuthority('sys:role:index')")
+    public CommonResult<Page<Role>> pageSearch(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "5") int pageSize, @RequestParam(required = false) String roleName) {
         LambdaQueryWrapper<Role> wrapper = new LambdaQueryWrapper<>();
         wrapper.like(roleName != null, Role::getRoleName, roleName);
         Page<Role> result = roleService.page(new Page<>(page, pageSize), wrapper);
         return CommonResult.success(result);
+    }
+
+    /**
+     * 添加角色信息
+     */
+    @PostMapping("/add")
+    @PreAuthorize("hasAuthority('sys:role:add')")
+    public CommonResult<String> addRole(@RequestBody Role role) {
+        return CommonResult.success("添加成功");
+    }
+
+    /**
+     * 编辑角色信息
+     */
+    @PutMapping("/update/{id}")
+    @PreAuthorize("hasAuthority('sys:role:update')")
+    public CommonResult<String> updateRole(@RequestBody Role role, @PathVariable String id) {
+        return CommonResult.success("修改成功");
+    }
+
+    /**
+     * 删除角色信息
+     */
+    @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasAuthority('sys:role:delete')")
+    public CommonResult<String> deleteRole(@PathVariable String id) {
+        throw new BizException("无法删除，正在被使用");
+//        return CommonResult.success("删除成功");
     }
 
 }
