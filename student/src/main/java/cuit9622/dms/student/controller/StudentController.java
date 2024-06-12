6b4d750921e62457310153b9c5c3ea7d01a60bc8
zxin <2932554779@qq.com>
2024-06-12 22:17:00 +0800

    feat:新增学生及分页total

--- student/src/main/java/cuit9622/dms/student/controller/StudentController.java
+++ student/src/main/java/cuit9622/dms/student/controller/StudentController.java
@@ -5,13 +5,13 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import cuit9622.dms.common.model.CommonResult;
 import cuit9622.dms.student.entity.Student;
 import cuit9622.dms.student.mapper.StudentMapper;
+import cuit9622.dms.student.service.StudentService;
 import cuit9622.dms.student.vo.StudentVo;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 
 @Slf4j(topic = "StudentController")
@@ -19,7 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
     @Resource
     private StudentMapper studentMapper;
+    @Resource
+    private StudentService studentService;
 
+    /**
+     * 获取学生分页信息
+     */
     @GetMapping("/list")
     public CommonResult<Page<StudentVo>> getStudents(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "5") int pageSize, @RequestParam(required = false) String name) {
         Page<StudentVo> result = new Page<>(page, pageSize);
@@ -32,5 +37,28 @@ public class StudentController {
         LambdaQueryWrapper<Student> wrapper = new LambdaQueryWrapper<>();
         wrapper.eq(Student::getStuId, id);
         return studentMapper.selectOne(wrapper);
+    }
+
+    /**
+     * 新增学生信息
+     */
+    @PostMapping("/add")
+    public CommonResult<String> add(@RequestBody Student student) {
+        boolean flag = studentService.save(student);
+        if(flag) {
+            return CommonResult.success("添加成功");
+        }
+        else return CommonResult.error(500,"添加失败");
+    }
+
+    /**
+     * 检查学号是否重复
+     */
+    @GetMapping("/check/{stuNum}")
+    public CommonResult<Boolean> checkUsername(@PathVariable String stuNum) {
+        LambdaQueryWrapper<Student> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(stuNum != null, Student::getStuNum,stuNum);
+        Student result = studentService.getOne(wrapper);
+        return CommonResult.success(Objects.isNull(result));
     }
 }
